496ffb238fc512b91cced5152f26e4c18ffd5f7a
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-18 16:16:03 -0500

    Setup of put route with update method. Updated delete route to return deleted user instead of message.

--- index.js
+++ index.js
@@ -49,19 +49,73 @@ server.get('/api/users/:id', (req, res) => {
     });
 });
 
+
 server.delete('/api/users/:id', (req, res) => {
     const userId = req.params.id;
     db
-    .remove(userId)
+    .findById(userId)
     .then(resp => {
-        if(!resp) {
+        if (!resp) {
             res.status(404).json({ message: 'The user with the specified ID does not exist.' })
-        }
-        res.status(200).json({message: `Deleted User: ${userId}`});
+        } 
+        res.status(200).json(resp);
+        db
+        .remove(userId)
+        .then(resp => {
+            if(!resp) {
+                res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The user could not be removed' })
+        });
     })
     .catch(() => {
         res.status(500).json({ error: 'The user could not be removed' })
     });
+});
+
+// server.delete('/api/users/:id', (req, res) => {
+//     const userId = req.params.id;
+//     db
+//     .remove(userId)
+//     .then(resp => {
+//         if(!resp) {
+//             res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+//         }
+//         res.status(200).json({message: `Deleted ${resp} User(s). Deleted User Id: #${userId}`});
+//     })
+//     .catch(() => {
+//         res.status(500).json({ error: 'The user could not be removed' })
+//     });
+// });
+
+server.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { name, bio, created_at, updated_at} = req.body;
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+        return
+    } else {
+        db
+        .update(userId, { name, bio, created_at, updated_at})
+        .then(resp => {
+            if(!resp) {
+                res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+            }
+            db
+            .findById(userId)
+            .then(resp => {
+                res.status(200).json(resp);
+            })
+            .catch(() => {
+                res.status(500).json({ error: 'The user information could not be retrieved.' })
+            });
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The user information could not be modified.' })
+        });
+    }
 });
 
 server.listen(8000, () => console.log('API running on port 8000'));
